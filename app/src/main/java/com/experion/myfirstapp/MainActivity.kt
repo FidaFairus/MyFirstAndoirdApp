5e7e3e7c4bd0ecf6ed6eb92665e29abf87b7a0be
FidaFairus <70806162+FidaFairus@users.noreply.github.com>
2023-05-04 15:02:25 +0530

    loginValidation

--- app/src/main/java/com/experion/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/experion/myfirstapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.experion.myfirstapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,12 +34,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -69,7 +78,21 @@ fun LoginPage(modifier: Modifier = Modifier, loginViewModel: LoginViewModel = Lo
     Column( modifier.padding(40.dp),
         verticalArrangement = Arrangement.Center,
     )
+
     {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var passwordVisible by rememberSaveable { mutableStateOf(false) }
+        val loginState by loginViewModel.loginStateLiveData.observeAsState(false)
+        var isEmailInvalid by remember { mutableStateOf(false) }
+        var isLoginSuccess by remember { mutableStateOf(false)}
+        var isLoginFailed by remember { mutableStateOf(false)}
+        Image(painter = painterResource(id = R.drawable.person), contentDescription = "person",
+        modifier= Modifier
+            .size(100.dp)
+            .clip(CircleShape)
+            .align(CenterHorizontally)
+        )
         Text(
             text="Login",
             style= TextStyle(
@@ -84,7 +107,7 @@ fun LoginPage(modifier: Modifier = Modifier, loginViewModel: LoginViewModel = Lo
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = "Username",
+            text = "Email",
             modifier = modifier,
             style= TextStyle(
                 fontSize = 16.sp,
@@ -93,18 +116,18 @@ fun LoginPage(modifier: Modifier = Modifier, loginViewModel: LoginViewModel = Lo
             )
         )
         Spacer(modifier = Modifier.height(8.dp))
-        var username by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var passwordVisible by rememberSaveable { mutableStateOf(false) }
-        val loginState by loginViewModel.loginStateLiveData.observeAsState(false)
 
         TextField(
-            value = username,
+            value = email,
             singleLine = true,
             onValueChange = { newText ->
-                username = newText
+                email = newText
+                isEmailInvalid=false
             },
-            label = {Text(text="Enter your Username")},
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email
+            ),
+            label = {Text(text="Enter valid Email")},
             modifier=Modifier.border(1.dp, Color.Gray, RoundedCornerShape(4.dp)),
             shape = RoundedCornerShape(4.dp),
             colors = TextFieldDefaults.textFieldColors(
@@ -116,6 +139,13 @@ fun LoginPage(modifier: Modifier = Modifier, loginViewModel: LoginViewModel = Lo
             ),
 
             )
+        if (isEmailInvalid) {
+            Text(
+                text = "Invalid email format",
+                color = Color.Red,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
         Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = "Password",
@@ -159,7 +189,19 @@ fun LoginPage(modifier: Modifier = Modifier, loginViewModel: LoginViewModel = Lo
         Spacer(modifier = Modifier.height(40.dp))
 
         Button(onClick = {
-            loginViewModel.isValidLoginData(username, password)
+            if(email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                    .matches()){
+
+                    isEmailInvalid = true // Set the error flag when the email is invalid
+                    return@Button
+
+            }
+            loginViewModel.isValidLoginData(email, password)
+            if (loginState) {
+                isLoginSuccess = true
+            } else {
+                isLoginFailed = true
+            }
         },
 
             colors = ButtonDefaults.buttonColors(Color.Black),
@@ -178,11 +220,13 @@ fun LoginPage(modifier: Modifier = Modifier, loginViewModel: LoginViewModel = Lo
             )
 
         }
-        if(loginState){
-            Text(text = "Login successfull")
-        }else{
-            Text(text = "Invalid username or password")
+        val context = LocalContext.current
+        if(isLoginSuccess){
+            Toast.makeText(context,"Login Successfull", Toast.LENGTH_SHORT).show()
         }
+//        else{
+//            Text(text = "Invalid username or password")
+//        }
 
 
 
--- app/src/main/java/com/experion/myfirstapp/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/experion/myfirstapp/viewmodel/LoginViewModel.kt
@@ -5,17 +5,19 @@ import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
     var userLiveData = MutableLiveData<String>()
+    var emailLiveData=MutableLiveData<String>()
     var passLiveData = MutableLiveData<String>()
     var userNameValue = "Admin"
+    var emailValue="admin@gmail.com"
     var passwordValue = "password@123"
     var loginStateLiveData = MutableLiveData<Boolean>()
     init {
-        userLiveData.value=""
+        emailLiveData.value=""
         passLiveData.value=""
     }
-    fun isValidLoginData(username : String, password: String){
-        if(username.isNotEmpty() && password.isNotEmpty()){
-            loginStateLiveData.postValue(userNameValue==username && password == passwordValue)
+    fun isValidLoginData(email : String, password: String){
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            loginStateLiveData.postValue(emailValue==email && password == passwordValue)
         }
     }
 }
